a89d0db2b778334ef6a9a0f8f6322c42c7c8e201
Taeyang Jin <sun@jupiterflow.com>
2021-02-15 17:30:57 +0900

    fix: Http2Protol Issue

--- src/main/java/kr/codefor/blog/config/WebConfig.java
+++ src/main/java/kr/codefor/blog/config/WebConfig.java
@@ -1,6 +1,9 @@
 package kr.codefor.blog.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.coyote.UpgradeProtocol;
+import org.apache.coyote.http2.Http2Protocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,5 +15,19 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://blog.codefor.kr");
+    }
+
+    @Bean
+    public TomcatConnectorCustomizer http2ProtocolCustomizer() {
+        return (connector) -> {
+            for (UpgradeProtocol upgradeProtocol: connector.findUpgradeProtocols()) {
+                if (upgradeProtocol instanceof Http2Protocol) {
+                    Http2Protocol http2Protocol = (Http2Protocol)upgradeProtocol;
+                    http2Protocol.setOverheadContinuationThreshold(0);
+                    http2Protocol.setOverheadDataThreshold(0);
+                    http2Protocol.setOverheadWindowUpdateThreshold(0);
+                }
+            }
+        };
     }
 }
