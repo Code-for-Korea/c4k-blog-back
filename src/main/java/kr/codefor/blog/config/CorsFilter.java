ea1632b909a835b833d167985d313304a40e7774
Taeyang Jin <sun@jupiterflow.com>
2021-02-15 16:23:42 +0900

    Update CorsFilter.java

--- src/main/java/kr/codefor/blog/config/CorsFilter.java
+++ src/main/java/kr/codefor/blog/config/CorsFilter.java
@@ -29,13 +29,13 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         }else {
-            chain.doFilter(req, res);\
+            chain.doFilter(req, res);
         }
     }
 }
